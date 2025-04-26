f8eae97a4dac701061e86393774f556ac0bef017
Hamzah <hamzahnafalah@gmail.com>
2025-04-26 17:54:16 +0700

    update schema package managements

--- src/Schemas/CardIdentity.php
+++ src/Schemas/CardIdentity.php
@@ -7,27 +7,9 @@ use Hanafalah\ModuleCardIdentity\Contracts\Schemas\CardIdentity as ContractsCard
 
 class CardIdentity extends PackageManagement implements ContractsCardIdentity
 {
-    protected array $__guard   = ['reference_id', 'reference_type'];
-    protected array $__add     = ['flag', 'value'];
     protected string $__entity = 'CardIdentity';
 
-    /**
-     * Add a new API access or update the existing one if found.
-     *
-     * The given attributes will be merged with the existing API access.
-     *
-     * @param array $attributes The attributes to be added to the API access.
-     *
-     * @return \Illuminate\Database\Eloquent\Model The API access model.
-     */
-    public function addOrChange(?array $attributes = []): self
-    {
-        if (isset($attributes['parent_model'])) {
-            $parent_model = $attributes['parent_model'];
-            $attributes['reference_id'] = $parent_model->getKey();
-            $attributes['reference_type'] = $parent_model->getMorphClass();
-        }
-        $this->updateOrCreate($attributes);
-        return $this;
+    public function prepareStoreCardIdentity(){
+        
     }
 }
