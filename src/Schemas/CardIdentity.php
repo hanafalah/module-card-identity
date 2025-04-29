f30a8c8066b7a3757be41fd0497a8b6d8f7c3e8d
Hamzah <hamzahnafalah@gmail.com>
2025-04-29 12:46:12 +0700

    update modules

--- src/Schemas/CardIdentity.php
+++ src/Schemas/CardIdentity.php
@@ -2,7 +2,6 @@
 
 namespace Hanafalah\ModuleCardIdentity\Schemas;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Hanafalah\ModuleCardIdentity\{
     Supports\BaseModuleCardIdentity
