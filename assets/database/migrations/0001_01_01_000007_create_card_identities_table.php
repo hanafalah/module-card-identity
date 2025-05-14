c99c3e0a7538163f0a5338d89f20e90d04975abb
Hamzah <hamzahnafalah@gmail.com>
2025-05-14 12:42:39 +0700

    update modules

--- assets/database/migrations/0001_01_01_000007_create_card_identities_table.php
+++ assets/database/migrations/0001_01_01_000007_create_card_identities_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Hanafalah\ModuleCardIdentity\Models\Identity\CardIdentity;
+use Hanafalah\ModuleCardIdentity\Models\CardIdentity;
 
 return new class extends Migration
 {
